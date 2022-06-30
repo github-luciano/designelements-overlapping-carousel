f8dd1888712520145cfe9cfed902ccd4fe8a1566
nluciano233 <nluciano233@gmail.com>
2022-06-30 13:57:50 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Overlapping carousel
+
+This is a carousel. Use and extension like live-sass to compile scss file into css.
---
+++ main.js
@@ -0,0 +1,51 @@
+const galleryContainer = document.querySelector('.gallery-container');
+const galleryControlsContainer = document.querySelector('.gallery-controls');
+const galleryControls = ['previous', 'add', 'next'];
+const galleryItems = document.querySelectorAll('.gallery-item');
+
+class Carousel {
+  constructor(container, items, controls) {
+    this.carouselContainer = container;
+    this.carouselControls = controls;
+    this.carouselArray = [...items];
+  };
+  
+  // update css classes for gallery
+  updateGallery() {
+    this.carouselArray.forEach(el => {
+      el.classList.remove('gallery-item-1');
+      el.classList.remove('gallery-item-2');
+      el.classList.remove('gallery-item-3');
+    });
+    
+    this.carouselArray.slice(0, 3).forEach((el, i) => {
+      el.classList.add(`gallery-item-${i+1}`);
+    });
+  };
+  
+  // update the current order of the carouselArray and gallery
+  setCurrentState(direction) {
+    if (direction.className == 'gallery-controls-previous') {
+      this.carouselArray.unshift(this.carouselArray.pop());
+    } else {
+      this.carouselArray.push(this.carouselArray.shift());
+    }
+    
+    this.updateGallery();
+  };
+  
+  useControls() {
+    const triggers = [...galleryControlsContainer.childNodes];
+    
+    triggers.forEach(control => {
+      control.addEventListener('click', e => {
+        e.preventDefault();
+        this.setCurrentState(control);
+      })
+    })
+  }
+  
+};
+
+const exampleCarousel = new Carousel(galleryContainer, galleryItems, galleryControls);
+exampleCarousel.useControls();
